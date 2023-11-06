88def6bcd0b4404992bfd145a82057967b9cbcbb
Amwaj-M <amwaj.mh3@gmail.com>
2023-11-06 15:11:52 +0300

    Add w5-d2-lab1

---
+++ script.js
@@ -0,0 +1,77 @@
+let screen = document.getElementById("screen");
+let img = document.getElementById("image");
+let but1 = document.getElementById("one");
+let but2 = document.getElementById("two");
+let but3 = document.getElementById("three");
+let but4 = document.getElementById("four");
+let but5 = document.getElementById("five");
+let but6 = document.getElementById("six");
+let but7 = document.getElementById("seven");
+let but8 = document.getElementById("eight");
+let but9 = document.getElementById("nine");
+
+
+function Chaneel1 () {
+    but1.addEventListener("click",function myFun() {
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+    })
+    
+}Chaneel1();
+
+function Chaneel2 () {
+    but2.addEventListener("click",function myFun() {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg" 
+    })
+    
+}Chaneel2();
+
+function Chaneel3 () {
+    but3.addEventListener("click",function myFun() {
+    img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+    })
+    
+}Chaneel3();
+
+function Chaneel4 () {
+    but4.addEventListener("click",function myFun() {
+    img.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+    })
+    
+}Chaneel4();
+
+function Chaneel5 () {
+    but5.addEventListener("click",function myFun() {
+    img.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+    })
+    
+}Chaneel5();
+
+function Chaneel6 () {
+    but6.addEventListener("click",function myFun() {
+    img.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+    })
+    
+}Chaneel6();
+
+function Chaneel7 () {
+    but7.addEventListener("click",function myFun() {
+        img.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+    })
+    
+}Chaneel7();
+
+function Chaneel8 () {
+    but8.addEventListener("click",function myFun() {
+    img.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+    })
+    
+}Chaneel8();
+
+function Chaneel9 () {
+    but9.addEventListener("click",function myFun() {
+    img.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+    })
+    
+}Chaneel9();
+
+
